f421695cb359cd56056b750b62d20f0c4d3536f0
Yuji Sasaki <jyu0414@gmail.com>
2019-10-30 03:37:45 +0900

    credential header

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ def index():
         response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Custom-Header')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
         return response
 
     return "error"
