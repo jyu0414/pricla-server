d856a0a4c6e3d6c2c36ea0699158dfb773a283f4
Yuji Sasaki <jyu0414@gmail.com>
2019-10-30 22:39:28 +0900

    add logo

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def index():
 
         # Init Layout
         docWidth = 1000
-        docHeight = 1408
+        docHeight = 1480
         xMargin = 50
         yMargin = 135
         xSep = 30
@@ -34,21 +34,26 @@ def index():
         response = make_response()
         reqImage = Image.open(BytesIO(request.get_data()))
         frame = Image.open("frame"+request.headers.get("frame")+".png")
+        icon = Image.open("logo.png")
 
         # Make Image
         img = Image.new('RGB', (docWidth, docHeight), (255, 255, 255))
         (reqW, reqH) = reqImage.size
         reqNewWidth = int(imgHeight/reqH*reqW)
-        reqImage = reqImage.resize((reqNewWidth, int(imgHeight)))
+        reqImage = reqImage.resize((reqNewWidth, int(imgHeight)),Image.BICUBIC)
         (frameW, frameH) = frame.size
         frameNewWidth = int(imgHeight/frameH*frameW)
-        frame = frame.resize((frameNewWidth, int(imgHeight)))
+        frame = frame.resize((frameNewWidth, int(imgHeight)),Image.BICUBIC)
+        (iconW,iconH) = icon.size
+        newIconWidth = int(50/iconH*iconW)
+        icon = icon.resize((newIconWidth,50),Image.BICUBIC)
         # crop
         reqImage = reqImage.crop(
             ((reqNewWidth - imgWidth)/2, 0, imgWidth + (reqNewWidth - imgWidth)/2, imgHeight))
         frame = frame.crop(((frameNewWidth - imgWidth)/2, 0,
                            imgWidth + (frameNewWidth - imgWidth)/2, imgHeight))
 
+        img.paste(icon,(xMargin,50),icon)
         for x in range(xCount):
             for y in range(yCount):
                 leftTopX = xMargin + x * (xSep + imgWidth)
